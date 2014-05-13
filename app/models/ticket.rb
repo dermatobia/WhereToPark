131d1847ec475f478fd8ed3b652745e1522668bb
dermatobia <webblewebble@gmail.com>
2014-05-13 15:40:11 -0400

    created models

---
+++ app/models/ticket.rb
@@ -0,0 +1,8 @@
+class Ticket < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :location
+
+  validates :user_id, presence: true
+  validates :location_id, presence: true
+  validates :issued_at, presence: true
+end 
