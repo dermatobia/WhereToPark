0cde228bfd343d56210e7fbaed0b1ca049f1d24e
dermatobia <webblewebble@gmail.com>
2014-05-13 14:29:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereToPark::Application.config.secret_key_base = '140a5ecb8320e0cbfba225ab5fc0b5bd6bfbb81e6a7193e6ade7bd5a5047c4169ac002b5f136de40040a0511357becaab12c997dfaa30687ca8e47973a4f5786'
