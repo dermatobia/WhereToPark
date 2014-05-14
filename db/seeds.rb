d5e42e31261b157b8b9a9bd501c5dcb14ebdd8a4
dermatobia <webblewebble@gmail.com>
2014-05-14 00:12:35 -0400

    initial seed to populate violation table based on csv file. create initial anonymous user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+# ========== POPULATE BASIC INFO TO VIOLATIONS TABLE ==================
+
+# CSV.foreach('./db/DOF_Parking_Violation_Codes.csv', :headers => true) do |obj|
+# 	code = obj[0].to_i
+# 	desc = obj[1]
+# 	Violation.create(cid: code, description: desc)
+# end
+
+# ========== Create Anonymous User ====================
+
+User.create( 	username: "Anonymous",
+							email: "zzz@zzz.com", 
+							password: "zzz",
+							password_confirmation: "zzz",
+							user_type: "A"
+							)
+
+ # => User(id: integer, username: string, email: string, password_digest: string, user_type: string, created_at: datetime, updated_at: datetime) 
