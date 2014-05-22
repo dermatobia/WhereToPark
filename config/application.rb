cf42a390cc8c6111bd4749b6642e28a32089959d
dermatobia <webblewebble@gmail.com>
2014-05-21 23:56:40 -0400

    add google map api key as ENV variable. gitignore the yml file

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,18 @@ Bundler.require(*Rails.groups)
 
 module WhereToPark
   class Application < Rails::Application
+
+    
+    # Set ENV variables from /config/local_env.yml file which contains API keys
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
