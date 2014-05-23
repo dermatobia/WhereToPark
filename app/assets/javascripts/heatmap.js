f5aded9d72b9e08c8e459a35d0178b951bfa5dd2
dermatobia <webblewebble@gmail.com>
2014-05-22 22:57:03 -0400

    test plotting multiple markers on map

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,11 +1,24 @@
+var home_loc = new google.maps.LatLng(40.7833, -73.9667);
+
+var marker1 = new google.maps.Marker({
+position:home_loc,
+});
+
+var marker2 = new google.maps.Marker({
+  position: {lat: 40.628151, lng: -73.972328}
+});
+
+
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8
+    center: home_loc,
+    zoom: 10
   };
-        
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-												        mapOptions);
+  
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  marker1.setMap(map);
+  marker2.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
